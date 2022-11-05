dc874256ab8d64b55edf15eb1056ff150f82e8b8
opn <opn@ad-host.ru>
2022-11-05 18:48:11 +0300

    Add check md5

--- vcf.py
+++ vcf.py
@@ -3,7 +3,37 @@
 import re
 import requests
 import configparser
+import hashlib
+from pathlib import Path
 
+#check md5sum file
+def check_md5():
+  file_name = "vc.vcf"
+  md5_file = "vc.md5"
+  md5_hash = hashlib.md5()
+  a_file = open("vc.vcf", "rb")
+  content = a_file.read()
+  md5_hash.update(content)
+  vcf_hash = md5_hash.hexdigest()
+
+  fle = Path(file_name)
+  fle.touch(exist_ok=True)
+
+  if Path(file_name).stat().st_size == 0:
+    return
+
+  fle = Path(md5_file)
+  fle.touch(exist_ok=True)
+
+  f = open(md5_file, 'r')
+  md5sum = f.read()
+  if md5sum == vcf_hash:
+    f.close()
+    exit()
+
+  f = open(md5_file, 'w')
+  f.write(vcf_hash)
+  f.close()
 
 # Read local file `config.ini`.
 config = configparser.ConfigParser()
@@ -15,6 +45,7 @@ yealink_xml = config['SETTINGS']['YEALINK_XML']
 resp = requests.get(url, auth=(user, password))
 open("vc.vcf", "wb").write(resp.content)
 
+check_md5()
 
 vcf = open("vc.vcf", "r")
 lines = vcf.readlines()
